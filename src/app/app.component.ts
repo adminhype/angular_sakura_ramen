42676b4a6fd081760877a69c8b577cb0cd0a9ca3
Adem <ado.designs@outlook.de>
2025-06-14 12:51:56 +0200

    feat(menu): add How to Order and Our Menu sections (HTML/CSS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from "./imprint/imprint.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MainContentComponent],
+  imports: [RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
